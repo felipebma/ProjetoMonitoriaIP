05a783698569427fbb18ecadfedc4a22b64a491e
Felipe Araújo <fbma@cin.ufpe.br>
2020-02-26 22:59:52 -0300

    Modularizando leitor de arquivo e criando o dataframe

--- Automovel.py
+++ Automovel.py
@@ -1,19 +1,49 @@
+import re
+
 class Automovel:
     def __init__(self, info = {}):
         self.categoria = info['categoria'] if 'categoria' in info else 'Não Informada'
-        self.modelo = info['modelo'] if 'modelo' in info else 'Não Informado'
-        self.tipo = info['tipo'] if 'tipo' in info else 'Não Informado'
-        self.marca = info['marca'] if 'marca' in info else 'Não Informada'
+        self.modelo = info['modelo'].title() if 'modelo' in info else 'Não Informado'
+        self.tipo = info['tipo de veículo'] if 'tipo de veículo' in info else 'Não Informado'
+        self.marca = info['marca'].title() if 'marca' in info else self.modelo.split()[0].title()
+        if self.marca == 'Gm - Chevrolet':
+            self.marca = 'Chevrolet'
         self.cidade = info['cidade'] if 'cidade' in info else 'Não Informada'
         self.ano = info['ano'].replace(' ','') if 'ano' in info else 'Não Informado'
-        self.km = info['quilometragem'] if 'quilometragem' in info else 'Não Informado'
+        self.km = int(info['quilometragem']) if 'quilometragem' in info else 'Não Informado'
         self.cor = info['cor'] if 'cor' in info else 'Não Informada'
         self.n_portas = info['portas'].split()[0] if 'portas' in info else 'Não Informado'
         self.conservacao = info['conservação'] if 'conservação' in info else 'Não Informada'
         self.combustivel = info['combustível'] if 'combustível' in info else 'Não Informado'
         self.final_da_placa = info['final de placa'] if 'final de placa' in info else 'Não Informado'
-        self.motor = info['motor'] if 'motor' in info else 'Não Informado'
+        self.motor = info['potência do motor'] if 'potência do motor' in info else 'Não Informado'
+        if self.motor=='Não Informado':
+            motor = re.search('[0-9].[0-9]',self.modelo)
+            if motor is not None:
+                self.motor = motor[0]
         self.transmissao = info['câmbio'] if 'câmbio' in info else 'Não Informada'
         self.n_fotos = info['fotos'] if 'fotos' in info else 'Não Informado'
         self.concessionaria = info['concessionaria'] if 'concessionaria' in info else 'Não Informada'
         self.preço = info['preço'] if 'preço' in info else 'Não Informado'
+
+    def getInfo(self):
+        return {
+            'Categoria':self.categoria,
+            'Modelo':self.modelo,
+            'Tipo':self.tipo,
+            'Marca':self.marca,
+            'Cidade':self.cidade,
+            'Ano':self.ano,
+            'Quilometragem':self.km,
+            'Cor':self.cor,
+            'Nº de Portas': self.n_portas,
+            'Conservação':self.conservacao,
+            'Combustível':self.combustivel,
+            'Final da Placa':self.final_da_placa,
+            'Potência do Motor':self.motor,
+            'Câmbio':self.transmissao,
+            'Nº de fotos':self.n_fotos,
+            'Concessionária':self.concessionaria,
+            'Preço':self.preço
+        }
+    
--- Main.py
+++ Main.py
@@ -3,62 +3,25 @@ import re
 import pandas as pd
 import matplotlib.pyplot as plt
 from Automovel import Automovel
-
-def readFile(file):
-    data = file.readlines()
-    info = {}    
-    if(data[0].strip()=='Categoria'):
-        info['categoria'] = data[1].strip()
-        for i in range(2,len(data)-1,2):
-            info[data[i].strip().lower()] = data[i+1].strip()
-    else:
-        info['modelo'] = data[0].strip()
-        info['cidade'] = data[1].strip()
-        text = ''
-        for l in data:
-            text += l
-        ano = re.search('ANO\n[0-9/\s]*',text)
-        if ano is not None:
-            info['ano'] = ano[0].strip().split('\n')[1]
-        km = re.search('KM\n[0-9]*',text)
-        if km is not None:
-            info['quilometragem'] = int(km[0].split('\n')[1])
-        cor = re.search('COR\n[a-zA-Z]*', text)
-        if cor is not None:
-            info['cor'] = cor[0].split('\n')[1]
-        portas = re.search('PORTAS\n[0-9]*',text)
-        if portas is not None:
-            info['portas'] = portas[0].split('\n')[1]
-        preço = re.search('R. [0-9,\.]*',text)
-        if preço is not None:
-            info['preço'] = preço[0]
-        conservação = re.search('Conservação: [a-zA-Z]*',text)
-        if conservação is not None:
-            info['conservação'] = conservação[0].split()[1]
-        combustível = re.search('Combustível: [a-zA-Z]*',text)
-        if combustível is not None:
-            info['combustível'] = combustível[0].split()[1]
-        placa = re.search('placa: [0-9]*',text)
-        if placa is not None:
-            info['final de placa'] = placa[0].split()[1]
-        motor = re.search('Motor: [0-9\.]*',text)
-        if motor is not None:
-            info['motor'] = motor[0].split()[1]
-        transmissão = re.search('Transmissão: [a-zA-Zá]*',text)
-        if transmissão is not None:
-            info['câmbio'] = transmissão[0].split()[1]
-        fotos = re.search('[0-9]* fotos',text)
-        if fotos is not None:
-            info['fotos'] = fotos[0].split()[0]
-        concessionaria = re.search('fotos\n[^\n]*',text)
-        if concessionaria is not None:
-            info['concessionaria'] = concessionaria[0].split('\n')[1]
-        
-    return Automovel(info)
+import Filereader
 
 
 with os.scandir('./inputs') as entries:
+    autos = []
     for entry in entries:
         with open(entry,'r') as f:
-            auto = readFile(f)
-            print(auto.concessionaria)
+            auto = Filereader.readFile(f)
+            autos.append(auto)
+    data = {}
+    for key in autos[0].getInfo():
+        data[key] = []
+    for auto in autos:
+        info = auto.getInfo();
+        for key in info:
+            data[key].append(info[key])
+
+dataframe = pd.DataFrame(data)
+
+# Gráfico pizza sobre as potências dos motores
+    
+    
